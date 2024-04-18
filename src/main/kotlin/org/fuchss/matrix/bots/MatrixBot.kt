a038d890b9b26c431297395bba9acbb4ce8dcb05
Dominik Fuchß <develop@fuchss.org>
2024-04-18 17:13:36 +0000

    Revert "Ensure that bot is only running in one instance"
    This reverts commit 0b1b9ec41d414fa8255907f483ca2ccb75a72a8a.

--- src/main/kotlin/org/fuchss/matrix/bots/MatrixBot.kt
+++ src/main/kotlin/org/fuchss/matrix/bots/MatrixBot.kt
@@ -50,8 +50,6 @@ class MatrixBot(private val matrixClient: MatrixClient, private val config: ICon
      * @return true if the bot was logged out, false if the bot simply quit.
      */
     suspend fun startBlocking(): Boolean {
-        check(running) { "Bot is already running!" }
-
         running = true
         registerShutdownHook()
 
