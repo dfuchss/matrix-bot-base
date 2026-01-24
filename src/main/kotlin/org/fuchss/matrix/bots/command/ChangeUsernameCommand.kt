9bf28af153a0e47548a07aa8729acd13bf5542c8
Dominik Fuchß <develop@fuchss.org>
2026-01-24 21:31:25 +0100

    Code review

--- src/main/kotlin/org/fuchss/matrix/bots/command/ChangeUsernameCommand.kt
+++ src/main/kotlin/org/fuchss/matrix/bots/command/ChangeUsernameCommand.kt
@@ -1,20 +1,30 @@
 package org.fuchss.matrix.bots.command
 
+import de.connect2x.trixnity.client.room.message.text
 import de.connect2x.trixnity.core.model.EventId
 import de.connect2x.trixnity.core.model.RoomId
 import de.connect2x.trixnity.core.model.UserId
 import de.connect2x.trixnity.core.model.events.m.room.RoomMessageEventContent
+import org.fuchss.matrix.bots.IConfig
 import org.fuchss.matrix.bots.MatrixBot
 import org.fuchss.matrix.bots.helper.canSendMessages
 import org.fuchss.matrix.bots.helper.isModerator
-import org.jetbrains.exposed.sql.Table.Dual.text
 
+/**
+ * Command to change the display name of the bot.
+ *
+ * When [globally] is `true`, the bot's display name is changed globally.
+ * When [globally] is `false`, the bot's display name is changed only for the given room.
+ *
+ * @param globally controls whether the name change applies globally or only in the current room.
+ */
 class ChangeUsernameCommand(
+    private val config: IConfig,
     private val globally: Boolean = false
 ) : Command() {
     override val name: String = "name"
     override val params: String = "{NEW_NAME}"
-    override val help: String = "sets the display name of the bot for this channel to NEW_NAME"
+    override val help: String = "sets the display name of the bot to NEW_NAME (for this channel, or globally when configured)"
 
     /**
      * Change the username of the bot.
@@ -43,9 +53,11 @@ class ChangeUsernameCommand(
             return
         }
 
-        if (globally) {
+        if (globally && config.isBotAdmin(sender)) {
             matrixBot.rename(parameters)
             return
+        } else if (globally) {
+            logger.info("User $sender tried to update global bot user name")
         }
 
         if (!sender.isModerator(matrixBot, roomId)) {
@@ -53,8 +65,6 @@ class ChangeUsernameCommand(
             return
         }
 
-        if (parameters.isNotBlank()) {
-            matrixBot.renameInRoom(roomId, parameters)
-        }
+        matrixBot.renameInRoom(roomId, parameters)
     }
 }
